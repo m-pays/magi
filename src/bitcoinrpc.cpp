debc53885fce468cbbd456a87e2ed012234f03d7
Joe-Lao <joelao98@gmail.com>
2014-09-20 22:27:22 -0400

    Testnet rpcport

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 19232 : 8232;
+    return GetBoolArg("-testnet", false) ? 18232 : 8232;
 }
 
 Object JSONRPCError(int code, const string& message)
