def9bac7383d424f8400f774b8ed6042b88fc866
Joe-Lao <joelao98@gmail.com>
2014-12-28 17:19:57 -0500

    Update gui

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1031,17 +1031,3 @@ void BitcoinGUI::updateMintingWeights()
     pwalletMain->GetStakeWeight(nMinWeight, nMaxWeight, nWeight);
     nNetworkWeight = GetPoSKernelPS();
 }
-
-void BitcoinGUI::updateMintingWeights_old()
-{
-    // Only update if we have the network's current number of blocks, or weight(s) are zero (fixes lagging GUI)
-    if ((clientModel && clientModel->getNumBlocks() == clientModel->getNumBlocksOfPeers()) || !nWeight || !nNetworkWeight)
-    {
-        nWeight = 0;
-
-        if (pwalletMain)
-            pwalletMain->GetStakeWeight(nMinWeight, nMaxWeight, nWeight);
-
-        nNetworkWeight = GetPoSKernelPS();
-    }
-}
