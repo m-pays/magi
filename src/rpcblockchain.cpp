8fbe6f10bec7d39272d2b7682ed3c4ececcab2c6
Joe-Lao <joelao98@gmail.com>
2015-01-01 22:25:54 -0500

    Fix API: getdifficulty

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -214,7 +214,7 @@ Value getblockcount(const Array& params, bool fHelp)
 }
 
 
-Value getdifficultym(const Array& params, bool fHelp)
+Value getdifficulty(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 0)
         throw runtime_error(
@@ -229,7 +229,7 @@ Value getdifficultym(const Array& params, bool fHelp)
 }
 
 
-Value getdifficulty(const Array& params, bool fHelp)
+Value getdifficultym(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 0)
         throw runtime_error(
