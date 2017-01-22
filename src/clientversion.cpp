522a4bb014d8ba7cb327fbecc151c62a6e221e88
joelao95 <ylao.bsd@gmail.com>
2017-01-21 22:13:43 -0500

    Remove duplicates

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,14 +45,6 @@ const std::string CLIENT_NAME("m-core");
 #    define GIT_COMMIT_DATE ""
 #endif
 
-
-#define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "" commit
-
-#define BUILD_DESC_FROM_UNKNOWN(maj,min,rev,build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) ""
-
-
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-" DO_STRINGIZE(suffix)
 
