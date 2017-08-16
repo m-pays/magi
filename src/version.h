5addb552e679a6bb99fd890bfc671d0a5deda667
joelao95 <joelao98@gmail.com>
2017-08-15 21:44:22 -0400

    Update version

--- src/version.h
+++ src/version.h
@@ -17,10 +17,10 @@ static const int DATABASE_VERSION = 72001;
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 71051;
+static const int PROTOCOL_VERSION = 71061;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 71030;
+static const int MIN_PROTO_VERSION = 71040;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
